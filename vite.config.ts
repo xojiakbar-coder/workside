7940e96a19e168a26ee20b0b25176480f8ed0dc1
xojiakbar coder <xojiakbarisroilov.777@gmail.com>
2025-01-12 19:17:09 +0500

    build command in vite

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  build: {
+    outDir: "build",
+  },
 });
